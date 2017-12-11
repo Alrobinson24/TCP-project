62b58f9004d73dc4f985d7c94d60b9996e1e6d07
Alrobinson24 <33476486+Alrobinson24@users.noreply.github.com>
2017-12-11 16:03:59 -0500

    Add files via upload

--- TCPserver.java
+++ TCPserver.java
@@ -1,80 +1,29 @@
-//**
-//author: ajrobinson0
 import java.io.*;
 import java.net.*;
-import java.util.Scanner;
-import java.net.ServerSocket;
-import java.net.Socket;
 
 public class TCPserver {
 
-	public static void main(String[] args) throws Exception {
-		// create a string method the provides the filename the server is sending to the client
-		String filename;
-		System.out.println("Enter the name of the Files:");
-		Scanner sc = new Scanner(System.in);
-		filename = sc.nextLine();
-		sc.close();
+	public static void main(String[] args) throws IOException {
+//Use the serverSocket to implement the connection between the client and put the file in the socket class
+		ServerSocket ss = new ServerSocket(1900);
+		Socket s = ss.accept();
+//Define the file as File.txt in the variable to be called transferFile and use the array method to find the data
+		System.out.println("Waiting for connection:" + socket);
+		File f = new File("File.txt");
+		byte [] bytearray  = new byte [(int)transferFile.length()];
+//The fin will be use to read the transferFile and the data would read the bytearray from the bin thats read the file 
+		FileInputStream fin = new FileInputStream(f);
+		BufferedInputStream bin = new BufferedInputStream(fin);
+		bin.read(bytearray,0,bytearray.length);
+// Define the outputstream to communicate with the client
+		OutputStream os = socket.getOutputStream();
+	    System.out.println("Sending the Files...");
+	    os.write(bytearray,0,bytearray.length);
+	    os.flush();
+		socket.close();
+		System.out.println("Transfering the file is complete");
 		
-		//create a loop method thats executes the ServerSocket that response to the port number (1900)
-		//print the dataprams of the input and output stream for the Serversocket's and socket's variables.
-		while(true) {
-			ServerSocket ss = new ServerSocket(1900);
-			System.out.println("Please wait processing the request");
-			Socket s = ss.accept();
-			System.out.println("Connnecting with"+ s.getInetAddress().toString());
-			DataInputStream dinput = new DataInputStream(s.getInputStream());
-			DataOutputStream doutput = new DataOutputStream(s.getOutputStream());
-			
-			//create a loop method that returns the string of 'str' that reads the input for the UTF
-			try {
-				String str = "";
-				str = dinput.readUTF();
-				System.out.println("Sending Files...");
-				
-				if(!str.equals("stop")) {
-					System.out.println("Sending the files:" +filename);
-					doutput.writeUTF(filename);
-					doutput.flush();
 					
-					File f = new File(filename);
-					FileInputStream fin = new FileInputStream(f);
-					
-					//create variables to read the long the method of 'mb' variable
-					long mb = (int) f.length();
-					byte b[] = new byte [1024];
-					int read;
-					
-					//write the UTF format for the output that returns the string and variable 
-					doutput.writeUTF(Long.toString(mb));
-					doutput.flush();
-					System.out.println("Size:" +mb);
-					System.out.println("Buffer size:" + ss.getReceiveBufferSize());
-					
-					// Use the loop method variable read the Dataoutput stream to write the data information in the file
-					while((read = fin.read(b)) != -1) {
-						doutput.write(b, 0, read);
-						doutput.flush();
-						
-						
-					}
-					fin.close();
-					System.out.println("ready..");
-					doutput.flush();
-					
-				}
-				doutput.writeUTF("STOP");
-				System.out.println("Sending the files is now complete");
-				doutput.flush();
-				
-			}catch(Exception e) {
-				e.printStackTrace();
-				System.out.println("There is an error");
-			}
-			dinput.close();
-			s.close();
-			ss.close();
-		}
 	}
 
 }
